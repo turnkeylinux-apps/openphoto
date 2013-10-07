ee9999c5fd7eb994f91cae482d1c7924308fe1ed
Alon Swartz <alon@turnkeylinux.org>
2013-10-07 16:05:13 +0300

    mysql password is now encrypted with secret (closes turnkeylinux/tracker#121)

--- overlay/usr/lib/inithooks/bin/openphoto_crypt.php
+++ overlay/usr/lib/inithooks/bin/openphoto_crypt.php
@@ -1,21 +1,22 @@
 <?php
-# encrypt/decrypt string (assumes blank secret, null salt)
+# encrypt/decrypt string (assumes null salt)
 
 define('PATH_LIBRARY', '/var/www/openphoto/src/libraries');
 include PATH_LIBRARY . '/models/Utility.php';
 
-if (count($argv)!=3) die("usage: $argv[0] <encrypt|decrypt> string\n");
+if (count($argv)!=4) die("usage: $argv[0] <encrypt|decrypt> string secret\n");
 
 $action = $argv[1];
 $string = $argv[2];
+$secret = $argv[3];
 $utility = new Utility();
 
 switch ($action) {
     case "encrypt":
-        print $utility->encrypt($string, "", null);
+        print $utility->encrypt($string, $secret, null);
         break;
     case "decrypt":
-        print $utility->decrypt($string, "", null);
+        print $utility->decrypt($string, $secret, null);
         break;
     default:
         die("supported actions: encrypt, decrypt");
